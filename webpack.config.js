cb5ba2c6e6a74c10b0b155302de5bf34bc525657
Seth Martineau <martineausw@gmail.com>
2024-11-24 16:53:10 -0500

    chore: add src boilerplate and initial webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.html',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
